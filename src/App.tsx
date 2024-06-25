e63e22da10bc12869eb586287ab6560db12533cd
Dan <daniel.smirnov17@gmail.com>
2024-06-25 21:07:13 +0100

    added styles to the App component and created Slider to control sorting speed

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,39 @@
+import { Slider } from "./components/Slider";
 import { useSortingAlgorithmContext } from "./context/Visualizer";
 
 export default function App() {
 
-  const { arrayToSort, isSorting } = useSortingAlgorithmContext();
+  const { arrayToSort, isSorting, animationSpeed, setAnimationSpeed } = useSortingAlgorithmContext();
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      {isSorting.toString()}
-    </h1>
-  )
+    <main className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
+      <div className="flex h-full justify-center">
+        <div id="content-container" className="flex max-w-[1020px] w-full flex-col lg:px-0 px-4">
+          <div className="h-[66px] relative flex items-center justify-between w-full">
+            <h1 className="text-gray-300 text-2xl font-light hidden md:flex">
+              Sorting Visulizer
+            </h1>
+            <div className="flex items-center justify-center gap-4">
+              <Slider
+                isDisabled={isSorting}
+                value={animationSpeed}
+                handleChange={(e) => setAnimationSpeed(Number(e.target.value))}
+              />
+            </div>
+          </div>
+          <div className="relative h-[calc(100vh-66px)] w-full">
+            <div className="absolute bottom-[32px] w-full mx-auto left-0 right-0 flex justify-center items-end">
+              {arrayToSort.map((value, index) => (
+                <div
+                  key={index}
+                  className="array-line relative w-1 mx-0.5 shadow-lg opacity-70 rounded-lg default-line-color"
+                  style={{ height: `${value}px` }}
+                ></div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
